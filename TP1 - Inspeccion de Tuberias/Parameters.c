9aa1c68ad64eefaa2eb8e06c6dc5136720e0aad1
Gaston <gastonmontes@hotmail.com>
2015-09-10 20:25:13 -0300

    Implement Parameters methods

--- TP1 - Inspeccion de Tuberias/Parameters.c
+++ TP1 - Inspeccion de Tuberias/Parameters.c
@@ -16,15 +16,30 @@ void setInvalidConfiguration(Parameters *parameters)
     parameters->routes_file_name = NULL;
 }
 
+void parseParameters(Parameters *parameters_parser, const char *parameters[]) {
+	parameters_parser->measures_file_name = parameters[1];
+	parameters_parser->pipes_file_name = parameters[2];
+	parameters_parser->routes_file_name = parameters[3];
+}
+
 void parametersCreate(Parameters *parameters_parser,
 					  int parameters_count,
 					  const char *parameters[]) {
 	setInvalidConfiguration(parameters_parser);
 
+	switch (parameters_count) {
+		case 4:
+			parseParameters(parameters_parser, parameters);
+			parameters_parser->parameters_code = ParametersCodeOK;
+			break;
+		default:
+			setInvalidConfiguration(parameters_parser);
+			break;
+	}
 }
 
 void parametersDestroy(Parameters *parameters_parser) {
-
+	setInvalidConfiguration(parameters_parser);
 }
 
 ParametersCode parametersCode(Parameters *parameters_parser) {
@@ -32,13 +47,13 @@ ParametersCode parametersCode(Parameters *parameters_parser) {
 }
 
 const char *parameterMeasuresFileName(Parameters *parameters_parser) {
-	return NULL;
+	return parameters_parser->measures_file_name;
 }
 
 const char *parameterPipesFileName(Parameters *parameters_parser) {
-	return NULL;
+	return parameters_parser->pipes_file_name;
 }
 
 const char *parameterRoutesFileName(Parameters *parameters_parser) {
-	return NULL;
+	return parameters_parser->routes_file_name;
 }
