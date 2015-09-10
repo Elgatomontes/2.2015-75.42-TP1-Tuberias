a48e07b973571907998cf41675b1eb973cc2efa7
Gaston <gastonmontes@hotmail.com>
2015-09-10 01:06:22 -0300

    Add Parameters files

---
+++ TP1 - Inspeccion de Tuberias/Parameters.c
@@ -0,0 +1,7 @@
+//
+//  Parameters.c
+//  TP1 - Inspeccion de Tuberias
+//
+//  Created by Gastón Montes on 9/9/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
---
+++ TP1 - Inspeccion de Tuberias/Parameters.h
@@ -0,0 +1,14 @@
+//
+//  Parameters.h
+//  TP1 - Inspeccion de Tuberias
+//
+//  Created by Gastón Montes on 9/9/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+
+
+#endif /* PARAMETERS_H_ */
