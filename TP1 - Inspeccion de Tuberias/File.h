08ae2795ef83f539fb6b956002d08a3b01f194aa
Gaston <gastonmontes@hotmail.com>
2015-09-12 15:46:34 -0300

    File header definition.

--- TP1 - Inspeccion de Tuberias/File.h
+++ TP1 - Inspeccion de Tuberias/File.h
@@ -9,6 +9,26 @@
 #ifndef FILE_H_
 #define FILE_H_
 
+#include <stdio.h>
 
+typedef enum {
+    FileOperationCodeSuccess,
+    FileOperationCodeFail,
+} FileOperationCode;
+
+typedef enum {
+    FileOpenModeRead,
+    FileOpenModeWrite
+} FileOpenMode;
+
+typedef struct {
+    FILE *file;
+    FileOperationCode operation_code;
+    int end_of_file;
+} File;
+
+void fileCreate(File *file, char *file_name, FileOpenMode open_mode);
+
+void fileDestroy(File *file);
 
 #endif /* FILE_H_ */
