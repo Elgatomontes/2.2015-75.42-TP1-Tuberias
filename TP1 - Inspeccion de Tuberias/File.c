69e39cb8b5cb76e883f269d8391e01f3b2a5e5d2
Gaston <gastonmontes@hotmail.com>
2015-09-12 15:59:03 -0300

    Implement File constructor and destructor

--- TP1 - Inspeccion de Tuberias/File.c
+++ TP1 - Inspeccion de Tuberias/File.c
@@ -6,4 +6,27 @@
 //  Copyright (c) 2015 Gastón Montes. All rights reserved.
 //
 
+#include "File.h"
 
+void fileCreate(File *file, char *file_name, FileOpenMode open_mode) {
+    file->file = NULL;
+    file->end_of_file = 0;
+    file->operation_code = FileOperationCodeFail;
+
+    if (file_name != NULL && strlen(file_name) > 0) {
+        file->file = fopen(file_name, openMode(open_mode));
+    }
+
+    if (file->file != NULL) {
+        file->operation_code = FileOperationCodeSuccess;
+    }
+}
+
+void fileDestroy(File *file) {
+    file->end_of_file = EOF;
+    file->operation_code = FileOperationCodeFail;
+
+    if (file->file != NULL) {
+        fclose(file->file);
+    }
+}
