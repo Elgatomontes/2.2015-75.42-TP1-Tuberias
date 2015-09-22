1d7c9db32045c69c2767f33bcf601f018b2d7137
Gaston <gastonmontes@hotmail.com>
2015-09-22 14:29:52 -0300

    Add pipes processor.

---
+++ TP1 - Inspeccion de Tuberias/PipesProcessor.h
@@ -0,0 +1,14 @@
+//
+//  PipesProcessor.h
+//  TP1 - Inspeccion de Tuberias
+//
+//  Created by Gastón Montes on 9/9/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#ifndef PIPESPROCESSOR_H_
+#define PIPESPROCESSOR_H_
+
+
+
+#endif /* PIPESPROCESSOR_H_ */
