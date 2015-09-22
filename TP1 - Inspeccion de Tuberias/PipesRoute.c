5e5a64c102d2be50791f3d4ce456f52ac13497e0
Gaston <gastonmontes@hotmail.com>
2015-09-22 12:59:25 -0300

    Add pipesRoute files.

---
+++ TP1 - Inspeccion de Tuberias/PipesRoute.c
@@ -0,0 +1,8 @@
+//
+//  PipesRoute.c
+//  TP1 - Inspeccion de Tuberias
+//
+//  Created by Gastón Montes on 9/9/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
---
+++ TP1 - Inspeccion de Tuberias/PipesRoute.h
@@ -0,0 +1,17 @@
+//
+//  PipesRoute.h
+//  TP1 - Inspeccion de Tuberias
+//
+//  Created by Gastón Montes on 9/9/15.
+//  Copyright (c) 2015 Gastón Montes. All rights reserved.
+//
+
+#ifndef PIPESROUTE_H_
+#define PIPESROUTE_H_
+
+typedef struct {
+
+
+} PipesRoute;
+
+#endif /* PIPESROUTE_H_ */
